5b7225e5ceb9273d67df1092381d5da451ac96ee
maicolkutchoukov <maicol.j.95@gmail.com>
2023-12-23 22:24:23 +0100

    Completato esercizio + bonus

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,10 @@
-console.log('Template html-css-js');
+const { createApp} = Vue;
+
+createApp({
+    data() {
+        return {
+            message: 'Hello World!',
+            img: 'https://nomadicsoft.io/wp-content/uploads/2023/03/vue-blog-cover-2.png'
+        }
+    }
+}).mount('#app');
